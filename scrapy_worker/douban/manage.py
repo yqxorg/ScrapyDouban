64eaa53c9826c4c03834cee9cdbc054f3c0a04a1
honglou <honglou2001@qq.com>
2018-12-30 21:44:04 +0800

    scrapy helloworld

--- scrapy_worker/douban/manage.py
+++ scrapy_worker/douban/manage.py
@@ -1,2 +1,3 @@
 from scrapy.cmdline import execute
-execute('scrapy crawl movie_meta'.split())
+# execute('scrapy crawl movie_meta'.split())
+execute('scrapy crawl to_scrape'.split())
--- scrapy_worker/douban/spiders/toscrape_test.py
+++ scrapy_worker/douban/spiders/toscrape_test.py
@@ -16,27 +16,37 @@ class Toscrapepider(CrawlSpider):
     start_urls = ['http://quotes.toscrape.com/']
     # rules = (
     #     Rule(LinkExtractor(allow=('book/subject/(\d).*rec$')),
-    #          callback='parse_item', follow=True, process_request='cookie'),
+    #          callback='parse_item', follow=False, process_request='cookie'),
     # )
 
-    def cookie(self, request):
-        bid = ''.join(random.choice(string.ascii_letters + string.digits) for
-                      x in range(11))
-        request.cookies['bid'] = bid
-        request = request.replace(url=request.url.replace('?', '/?'))
-        return request
+    # def cookie(self, request):
+    #     bid = ''.join(random.choice(string.ascii_letters + string.digits) for
+    #                   x in range(11))
+    #     request.cookies['bid'] = bid
+    #     request = request.replace(url=request.url.replace('?', '/?'))
+    #     return request
 
     def start_requests(self):
         for url in self.start_urls:
             bid = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(11))
             yield Request(url, cookies={'bid': bid})
 
-    def get_douban_id(self, subject, response):
-        subject['douban_id'] = response.url[34:-10]
-        return subject
+    # def get_douban_id(self, subject, response):
+    #     subject['douban_id'] = response.url[34:-10]
+    #     return subject
 
-    def parse_item(self, response):
-        subject = Subject()
-        self.get_douban_id(subject, response)
-        subject['type'] = 'book'
-        return subject
+    def parse(self, response):
+        '''
+        默认回调
+        :param response:
+        :return:
+        '''
+        text = response.xpath("/html/body/div/div[2]/div[1]/div[4]/span[1]/text()").extract()
+        print(text)
+
+    #
+    # def parse_item(self, response):
+    #     subject = Subject()
+    #     self.get_douban_id(subject, response)
+    #     subject['type'] = 'book'
+    #     return subject
